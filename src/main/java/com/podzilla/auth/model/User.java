9e3546f5c6f51238ebe065b32b0f990ae3d9962c
Nour Eldien Ayman <nourayman189@gmail.com>
2025-05-14 17:28:21 +0300

    Remove unused validation annotations from Address and User classes

--- src/main/java/com/podzilla/auth/model/User.java
+++ src/main/java/com/podzilla/auth/model/User.java
@@ -19,8 +19,6 @@ import java.util.Set;
 import java.util.UUID;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
