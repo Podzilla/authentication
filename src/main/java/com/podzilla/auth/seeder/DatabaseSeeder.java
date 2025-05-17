faa934bb31bf4242744c59bde7327d2543b24542
Nour Eldien Ayman <nourayman189@gmail.com>
2025-05-17 23:16:19 +0300

    Refactor DatabaseSeeder to include repositories for user, address, and refresh tokens; clear user roles and delete all users during seeding

--- src/main/java/com/podzilla/auth/seeder/DatabaseSeeder.java
+++ src/main/java/com/podzilla/auth/seeder/DatabaseSeeder.java
@@ -2,7 +2,10 @@ package com.podzilla.auth.seeder;
 
 import com.podzilla.auth.model.ERole;
 import com.podzilla.auth.model.Role;
+import com.podzilla.auth.repository.AddressRepository;
+import com.podzilla.auth.repository.RefreshTokenRepository;
 import com.podzilla.auth.repository.RoleRepository;
+import com.podzilla.auth.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -10,14 +13,30 @@ import org.springframework.stereotype.Component;
 public class DatabaseSeeder implements CommandLineRunner {
 
     private final RoleRepository roleRepository;
+    private final RefreshTokenRepository refreshTokenRepository;
+    private final UserRepository userRepository;
+    private final AddressRepository addressRepository;
 
-    public DatabaseSeeder(final RoleRepository roleRepository) {
+    public DatabaseSeeder(final RoleRepository roleRepository,
+                          final RefreshTokenRepository refreshTokenRepository,
+                          final UserRepository userRepository,
+                          final AddressRepository addressRepository) {
         this.roleRepository = roleRepository;
+        this.refreshTokenRepository = refreshTokenRepository;
+        this.userRepository = userRepository;
+        this.addressRepository = addressRepository;
     }
 
     @Override
     public void run(final String... args) throws Exception {
+        userRepository.findAll().forEach(user -> {
+            user.getRoles().clear();
+            userRepository.save(user);
+        });
+        userRepository.deleteAll();
         roleRepository.deleteAll();
+        refreshTokenRepository.deleteAll();
+        addressRepository.deleteAll();
         roleRepository.save(new Role(ERole.ROLE_USER));
         roleRepository.save(new Role(ERole.ROLE_ADMIN));
         roleRepository.save(new Role(ERole.ROLE_COURIER));
